9fcc669cbdf5dda83dba1184ded273d5af4d11a8
Mariana Goncalves <mari.goncalves004@gmail.com>
2024-07-06 01:25:19 -0300

    Classe Vaga criada

---
+++ testeA3/src/Vaga.java
@@ -0,0 +1,17 @@
+public class Vaga {
+    private String titulo;
+    // private DescricaoVaga descricaoVaga; 
+    private Empresa empresa; 
+    private Recrutador recrutador; 
+    // private EstadoVaga estadoVaga; 
+
+    public void ExibirVaga(){
+
+    }
+    public void EditarVaga(){
+
+    }
+    public void ExcluirVaga(){
+
+    }
+}
