9926c8c26877112842c025f134ad01bb98b47f81
Mariana Goncalves <mari.goncalves004@gmail.com>
2024-07-06 16:03:54 -0300

    método MenuCandidato populado

--- testeA3/src/Candidato.java
+++ testeA3/src/Candidato.java
@@ -1,10 +1,18 @@
+import java.util.Scanner;
+
 public class Candidato extends Pessoa {
     private String endereco;
     private String cpf;
-    private Curriculo curriculo; 
+    private Curriculo curriculo;
 
-// *Como puxar enum?*
-// -ArrayList<Vagas>: Vaga
+    Scanner sc = new Scanner(System.in);
+
+    // *Como puxar enum?*
+    // -ArrayList<Vagas>: Vaga
+
+    public Candidato() {
+        MenuCandidato();
+    }
 
     public String getEndereco() {
         return endereco;
@@ -22,16 +30,60 @@ public class Candidato extends Pessoa {
         this.cpf = cpf;
     }
 
-    public void PesquisarVagas(){
+    public void MenuCandidato() {
+        Atalhos atalhos = new Atalhos();
+
+        System.out.print("Olá candidato(a)! Você deseja: \n");
+
+        atalhos.PularLinha();
+
+        atalhos.EscreverMensagem("1-Criar currículo \n");
+        atalhos.EscreverMensagem("2-Atualizar curriculo \n");
+        atalhos.EscreverMensagem("3-Ver vagas disponiveis \n");
+        atalhos.EscreverMensagem("4-Listar candidaturas \n");
+
+        atalhos.PularLinha();
+
+        atalhos.EscreverMensagem("Escolha uma opção: ");
+        int opcaoMenuCandidato = sc.nextInt();
+
+        switch (opcaoMenuCandidato) {
+            case 1:
+                CriarCurriculo();
+                break;
+            case 2:
+                AtualizarCurriculo();
+                break;
+            case 3:
+                ListarVagasDisponiveis();
+                break;
+            case 4:
+                ListarCandidaturas();
+                break;
+
+            default:
+                atalhos.EscreverMensagem("Valor inválido! Tente novamente. \n");
+                break;
+        }
 
     }
-    public void CriarCurriculo(){
+    // Acho que o candidato só podeá ver uma lista com vagas, sem pesquisar.
+    // public void PesquisarVagas() {
+
+    // }
+
+    public void CriarCurriculo() {
+        // Curriculo curriculoCandidato = new Curriculo();
+    }
+
+    public void AtualizarCurriculo() {
 
     }
-    public void AtualizarCurriculo(){
+
+    public void ListarVagasDisponiveis() {
 
     }
-    public void ListarVagas(){
+    public void ListarCandidaturas() {
 
     }
 
