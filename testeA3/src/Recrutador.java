f10c919a1729d5ec2a4a9d21a2b0637319291316
Mariana Goncalves <mari.goncalves004@gmail.com>
2024-07-06 01:13:02 -0300

    Classe Recrutador criada

---
+++ testeA3/src/Recrutador.java
@@ -0,0 +1,43 @@
+public class Recrutador extends Pessoa {
+    private String cargo;
+    private String empresa; 
+
+   
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+
+    public void SelecionarCandidato(){
+
+    }
+    public void ExcluirCandidato(){
+
+    }
+    public void ListarCandidaturas(){
+
+    }
+    public void EditarStatusCandidatura(){
+
+    }
+    public void ListarVagas(){
+
+    }
+    public void CadastrarEmpresa(){
+
+    }
+    public void AgendarEntrevista(){
+
+    }
+}
