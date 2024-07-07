bb7f0a85933af7150152ad0820b858db9c47bd46
Mariana Goncalves <mari.goncalves004@gmail.com>
2024-07-07 01:23:59 -0300

    Refatoração da estrutura

--- testeA3/src/Candidato.java
+++ testeA3/src/Candidato.java
@@ -10,10 +10,78 @@ public class Candidato extends Pessoa {
 
     Scanner sc = new Scanner(System.in);
     Curriculo curriculoCandidato = new Curriculo();
+    boolean isCurriculoCriado = false;
+    Atalhos atalhos = new Atalhos();
+    int opcaoMenuCandidato;
 
     // Método construtor - 1º método chamado após a classe ser intanciada.
     public Candidato() {
-        MenuCandidato();
+        atalhos.EscreverMensagem("Olá candidato(a)! Você deseja: \n");
+        atalhos.PularLinha();
+        ExibirMenuCandidato();
+    }
+
+    public void ExibirOpcoesMenuCandidato(){
+        atalhos.EscreverMensagem("1-Criar currículo \n");
+        atalhos.EscreverMensagem("2-Atualizar curriculo \n");
+        atalhos.EscreverMensagem("3-Ver vagas disponiveis \n");
+        atalhos.EscreverMensagem("4-Listar candidaturas \n");
+
+        atalhos.PularLinha();
+    }
+
+    public void ValidarOpcaoMenuCandidato(){
+        atalhos.EscreverMensagem("Escolha uma opção: ");
+        opcaoMenuCandidato = sc.nextInt();
+
+        switch (opcaoMenuCandidato) {
+            case 1:
+                if (!isCurriculoCriado) {
+                    CriarCurriculo();
+                } else {
+                    atalhos.EscreverMensagem("Seu curriculo já foi criado. \n");
+                }
+                break;
+
+            case 2:
+                AtualizarCurriculo();
+                break;
+
+            case 3:
+                ListarVagasDisponiveis();
+                break;
+
+            case 4:
+                ListarCandidaturas();
+                break;
+
+            default:
+                atalhos.EscreverMensagem("Valor inválido! Tente novamente. \n");
+                break;
+        }
+    }
+
+    public void ExibirMenuCandidato() {
+        ExibirOpcoesMenuCandidato();
+        ValidarOpcaoMenuCandidato();
+    }
+
+    public void CriarCurriculo() {
+        curriculoCandidato.CriarCurriculo();
+        isCurriculoCriado = true;
+        ExibirMenuCandidato();
+    }
+
+    public void AtualizarCurriculo() {
+
+    }
+
+    public void ListarVagasDisponiveis() {
+
+    }
+    
+    public void ListarCandidaturas() {
+
     }
 
     public String getEndereco() {
@@ -30,64 +98,5 @@ public class Candidato extends Pessoa {
 
     public void setCpf(String cpf) {
         this.cpf = cpf;
-    }
-
-    public void MenuCandidato() {
-        Atalhos atalhos = new Atalhos();
-
-        atalhos.EscreverMensagem("Olá candidato(a)! Você deseja: \n");
-
-        atalhos.PularLinha();
-
-        atalhos.EscreverMensagem("1-Criar currículo \n");
-        atalhos.EscreverMensagem("2-Atualizar curriculo \n");
-        atalhos.EscreverMensagem("3-Ver vagas disponiveis \n");
-        atalhos.EscreverMensagem("4-Listar candidaturas \n");
-
-        atalhos.PularLinha();
-
-        atalhos.EscreverMensagem("Escolha uma opção: ");
-        int opcaoMenuCandidato = sc.nextInt();
-
-        switch (opcaoMenuCandidato) {
-            case 1:
-                CriarCurriculo();
-                break;
-            case 2:
-                AtualizarCurriculo();
-                break;
-            case 3:
-                ListarVagasDisponiveis();
-                break;
-            case 4:
-                ListarCandidaturas();
-                break;
-
-            default:
-                atalhos.EscreverMensagem("Valor inválido! Tente novamente. \n");
-                break;
-        }
-
-        sc.close();
-
-    }
-    // Acho que o candidato só podeá ver uma lista com vagas, sem pesquisar.
-    // public void PesquisarVagas() {
-
-    // }
-
-    public void CriarCurriculo() {
-        curriculoCandidato.CriarCurriculo();
-    }
-
-    public void AtualizarCurriculo() {
-
-    }
-
-    public void ListarVagasDisponiveis() {
-
-    }
-    public void ListarCandidaturas() {
-
     }
 }
